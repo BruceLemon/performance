4746b5b055b9d781d8e4705eb948c5fa23b3d5bb
niuqp <asiwzt@gmail.com>
2018-11-23 14:54:02 +0800

    支行行长绩效考核界面模型初版1.02

--- client/web/performance/js/page/presidentOfBranch.js
+++ client/web/performance/js/page/presidentOfBranch.js
@@ -12,25 +12,28 @@
                 $("#imgMinePerformance").attr("src","images/icon/page/wode2.png");
                 $("#imgMer1Performance").attr("src","images/icon/page/gongsi1.png");
                 $("#imgMer2Performance").attr("src","images/icon/page/lingshou2.png");
-                $("#textMinePerformance").attr("style","color:#3d4954");
-                $("#textMer1Performance").attr("style","color:#fe6600");
-                $("#textMer2Performance").attr("style","color:#3d4954");
+                $("#textMinePerformance").attr("style","color:#3d4954;margin-left:5px;font-size: 16px;");
+                $("#textMer1Performance").attr("style","color:#fe6600;margin-left:5px;font-size: 16px;");
+                $("#textMer2Performance").attr("style","color:#3d4954;margin-left:5px;font-size: 16px;");
+                $("#underline").attr("style","margin-top:40px;margin-left:110px;width:123px;height:10px");
             });
             $(document).on('click', "#presidentTabMembers2", function () {
                 $("#imgMinePerformance").attr("src","images/icon/page/wode2.png");
                 $("#imgMer1Performance").attr("src","images/icon/page/gongsi2.png");
                 $("#imgMer2Performance").attr("src","images/icon/page/lingshou1.png");
-                $("#textMinePerformance").attr("style","color:#3d4954");
-                $("#textMer1Performance").attr("style","color:#3d4954");
-                $("#textMer2Performance").attr("style","color:#fe6600");
+                $("#textMinePerformance").attr("style","color:#3d4954;margin-left:5px;font-size: 16px;");
+                $("#textMer1Performance").attr("style","color:#3d4954;margin-left:5px;font-size: 16px;");
+                $("#textMer2Performance").attr("style","color:#fe6600;margin-left:5px;font-size: 16px;");
+                $("#underline").attr("style","margin-top:40px;margin-left:250px;width:123px;height:10px");
             });
             $(document).on('click', "#presidentTabMine", function () {
                 $("#imgMinePerformance").attr("src","images/icon/page/wode1.png");
                 $("#imgMer1Performance").attr("src","images/icon/page/gongsi2.png");
                 $("#imgMer2Performance").attr("src","images/icon/page/lingshou2.png");
-                $("#textMinePerformance").attr("style","color:#fe6600");
-                $("#textMer1Performance").attr("style","color:#3d4954");
-                $("#textMer2Performance").attr("style","color:#3d4954");
+                $("#textMinePerformance").attr("style","color:#fe6600;margin-left:5px;font-size: 16px;");
+                $("#textMer1Performance").attr("style","color:#3d4954;margin-left:5px;font-size: 16px;");
+                $("#textMer2Performance").attr("style","color:#3d4954;margin-left:5px;font-size: 16px;");
+                $("#underline").attr("style","margin-top:40px;margin-left:6px;width:88px;height:10px");
             });
 
             myApp.onPageAfterAnimation('role1', function (page) {
